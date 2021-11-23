376b04f9833fe030f906dda6798eee57c16645cf
sbriffaud <sbriffaud@LAPTOP-K0VA6E62.localdomain>
2021-11-23 09:25:00 +0100

    [v1.0] Implémentation de Tools, méthodes vides

---
+++ EasySaveConsole/EasySaveConsole/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySaveConsole
+{
+    class Tools
+    {
+        public Tools() { }
+
+        public int getFileSize(Uri path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean checkAccess(Uri path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean checkExistance(Uri path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
