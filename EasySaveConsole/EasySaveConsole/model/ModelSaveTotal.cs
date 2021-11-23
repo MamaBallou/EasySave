7dfbe037ca02b0b291a610d5cc3d3aec220b8f20
Brenda <74040397+BrendaDJIGUEMDE@users.noreply.github.com>
2021-11-23 23:55:16 +0100

    Update ModelSaveTotal.cs

--- EasySaveConsole/EasySaveConsole/model/ModelSaveTotal.cs
+++ EasySaveConsole/EasySaveConsole/model/ModelSaveTotal.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*récupérer tous les fichiers texte d’un répertoire et de ses sous-répertoires, puis les déplacer vers un nouveau répertoire. 
+ Une fois les fichiers déplacés, ils n’existent plus dans les répertoires d’origine.*/
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,13 +9,33 @@ namespace EasySaveConsole.model
 {
     class ModelSaveTotal : ModelSave
     {
-        public ModelSaveTotal(string name, string sourceFile, string targetFile) : base(name, sourceFile, targetFile)
-        {
-        }
-
         public override void save()
         {
-            throw new NotImplementedException();
+          
+                bool sourceExists = ModelSaveTotal.sourceFile.Exists();
+                bool targetExists = ModelSaveTotal.targetFile.Exixts();
+                if (sourceExists & targetExists)
+                {
+                    try
+                    {
+                        var TotalFiles = Directory.EnumerateFiles(sourceFile);
+                        foreach (string currentFile in TotalFiles)
+                        {
+                            string fileName = currentFile.Substring(sourceFile.Length + 1);
+                            Directory.Move(currentFile, Path.Combine(targetFile, fileName));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR!!! Please verify directories syntax, they not exist ");
+                }
+            
+
         }
     }
 }
