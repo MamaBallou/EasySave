739bd3efa145d97949b4a86218e2418c8122f6ce
Coralistone <94457044+Coralistone@users.noreply.github.com>
2021-11-24 09:42:48 +0100

    Tools implementation beginings

--- EasySaveConsole/EasySaveConsole/Tools.cs
+++ EasySaveConsole/EasySaveConsole/Tools.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EasySaveConsole
 {
     public class Tools
     {
-        private static Tools uniqueInstance;
-        public static Tools getInstance()
-        {
-            return uniqueInstance;
-        }
-        private Tools() { }
+        public static int Lenght { get; private set; }
+
+        public Tools() { }
 
         public int getFileSize(Uri path)
         {
-            throw new NotImplementedException();
-
+            FileInfo fi = new FileInfo(@path.ToString());
+            //int resultat = FileInfo.Lenght;
+            int resultat = (int) fi.Length;
+            return resultat;
         }
+
+
 
         public Boolean checkAccess(Uri path)
         {
+            /*
+            //throw new NotImplementedException();
+            Boolean resultat = File.ReadAllText("test_files.txt");
+            Boolean resultat = File.ReadAllText("String path");
+            return resultat;
+            */
             throw new NotImplementedException();
         }
 
+
+
         public Boolean checkExistance(Uri path)
+        {
+            //throw new NotImplementedException();
+            Boolean res = File.Exists(path.ToString());
+            return res;
+        }
+
+        public bool testcheckAccess(Uri path)
         {
             throw new NotImplementedException();
         }
