19bf2b0087e62c998a479cd3b987d1b99d897e45
Mathilde Ballouhey <mathildeballouhey.nausikaa44@gmail.com>
2021-11-25 00:11:05 +0100

    Cleaning ModelSave classes

--- EasySaveConsole/EasySaveConsole/model/ModelSave.cs
+++ EasySaveConsole/EasySaveConsole/model/ModelSave.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using EasySaveConsole.controller;
+using EasySaveConsole.exception;
+using EasySaveConsole.logger;
+using System;
+using System.IO;
 
 namespace EasySaveConsole.model
 {
     public abstract class ModelSave
     {
+        private LogLogger log = LogLogger.getInstance();
+        private StateLogger stateLogger = StateLogger.getInstance();
         protected string name;
         protected string sourceFile;
         protected string targetFile;
@@ -15,29 +21,45 @@ namespace EasySaveConsole.model
             this.targetFile = targetFile;
         }
 
-        public virtual void save(ref ModelState modelState)
+        protected void save(ref ModelState modelState)
+        {
+            //define source and target path in bool
+            Tool tool = Tool.getInstance();
+            bool sourceExists = tool.checkExistance(sourceFile);
+            bool targetExists = tool.checkExistance(targetFile);
+
+            if (!sourceExists)
+            {
+                //Exception if source doesn't exist do exception
+                throw new PathNotFoundException();
+            }
+
+            // get the file attributes for file or directory
+            FileAttributes attr = File.GetAttributes(sourceFile);
+
+            modelState.State = State.OnGoing;
+            stateLogger.write(ControllerSave.modelStates);
+            //detect whether its a directory or file
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+            {
+                var TotalFiles = Directory.EnumerateFiles(sourceFile);
+                //for each file in Directory, copy this.
+                foreach (string currentFile in TotalFiles)
+                {
+                    saveAFile(ref modelState, currentFile);
+                }
+            }
+            else
+            {
+                saveAFile(ref modelState, sourceFile);
+            }
+            modelState.State = State.Finish;
+            stateLogger.write(ControllerSave.modelStates);
+        }
+
+        protected virtual void saveAFile(ref ModelState modelState, string currentFile)
         {
             throw new NotImplementedException();
-        }
-
-        public virtual ModelState toModelState()
-        {
-            throw new NotImplementedException();
-        }
-
-        public virtual ModelLog toModelLog()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string getSourcePath()
-        {
-            return sourceFile;
-        }
-
-        public string getTargetPath()
-        {
-            return targetFile;
         }
     }
 }
--- EasySaveConsole/EasySaveConsole/model/ModelSaveDifferential.cs
+++ EasySaveConsole/EasySaveConsole/model/ModelSaveDifferential.cs
@@ -12,46 +12,9 @@ namespace EasySaveConsole.model
         private StateLogger stateLogger = StateLogger.getInstance();
         public ModelSaveDifferential(string name, string sourceFile,
             string targetFile) : base(name, sourceFile, targetFile)
-        {
-        }
+        { }
 
-        public override void save(ref ModelState modelState)
-        {
-            //define source and target path in bool
-            Tool tool = Tool.getInstance();
-            bool sourceExists = tool.checkExistance(sourceFile);
-            bool targetExists = tool.checkExistance(targetFile);
-
-            if (!sourceExists)
-            {
-                //Exception if source doesn't exist do exception
-                throw new PathNotFoundException();
-            }
-
-            // get the file attributes for file or directory
-            FileAttributes attr = File.GetAttributes(sourceFile);
-
-            modelState.State = State.OnGoing;
-            stateLogger.write(ControllerSave.modelStates);
-            //detect whether its a directory or file
-            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
-            {
-                var TotalFiles = Directory.EnumerateFiles(sourceFile);
-                //for each file in Directory, copy this.
-                foreach (string currentFile in TotalFiles)
-                {
-                    saveAFile(ref modelState, currentFile);
-                }
-            }
-            else
-            {
-                saveAFile(ref modelState, sourceFile);
-            }
-            modelState.State = State.Finish;
-            stateLogger.write(ControllerSave.modelStates);
-        }
-
-        private void saveAFile(ref ModelState modelState, string currentFile)
+        protected override void saveAFile(ref ModelState modelState, string currentFile)
         {
             string fileName = Path.GetFileName(currentFile.ToString());
             string currentTarget = String.Concat(targetFile, "/", fileName);
