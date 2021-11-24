86842ed05624e2dac4624c9b22dfe783443add67
sbriffaud <sbriffaud@LAPTOP-K0VA6E62.localdomain>
2021-11-24 20:59:19 +0100

    [v1] Implémentation des méthodes de Logger pour une sauvegarde complète d'un dossier + écriture des logs

--- EasySaveConsole/EasySaveConsole/logger/LogLogger.cs
+++ EasySaveConsole/EasySaveConsole/logger/LogLogger.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using EasySaveConsole.model;
 
 namespace EasySaveConsole.logger
 {
-    class LogLogger : Logger
+    public sealed class LogLogger
     {
-        public override void Write(string data)
+        private static string PATH = @"..\..\..\logs\";
+        private static LogLogger _instance;
+
+        private LogLogger() { }
+
+        public static LogLogger getInstance()
         {
-            throw new NotImplementedException();
+            if (_instance == null)
+            {
+                _instance = new LogLogger();
+            }
+            return _instance;
+        }
+
+        public String getPATH()
+        {
+            return LogLogger.PATH;
+        }
+
+        public void write(ModelSave[] data)
+        {
+            int i = 0;
+
+            foreach (ModelSave ms in data)
+            {
+                if(ms.GetType().Name == "ModelSaveTotal")
+                {
+                    foreach (string file in Directory.GetFiles(ms.getSourcePath()))
+                    {
+                        string[] lines = File.ReadAllLinesAsync(file).Result;
+
+                        File.WriteAllLinesAsync(ms.getTargetPath() + Path.GetFileName(file), lines);
+
+                        ModelLog ml = ms.toModelLog(ms.getSourcePath() + Path.GetFileName(file), i, ms.getTargetPath() + Path.GetFileName(file));
+                        File.AppendAllLinesAsync(PATH + "log" + i + ".txt", ml.toString());
+                    }
+                }
+                else if(ms.GetType().Name == "ModelSaveDifferential")
+                {
+                    Console.WriteLine("ModelSaveDifferential");
+                }
+                i++;
+            }
         }
     }
 }
