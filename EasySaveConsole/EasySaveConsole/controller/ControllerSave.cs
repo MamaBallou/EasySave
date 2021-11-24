fd162ec8e2e5ee26a3ec143c5672d0374212713e
sbriffaud <sbriffaud@LAPTOP-K0VA6E62.localdomain>
2021-11-24 20:55:15 +0100

    [v1] Ajout controllers

--- EasySaveConsole/EasySaveConsole/controller/ControllerSave.cs
+++ EasySaveConsole/EasySaveConsole/controller/ControllerSave.cs
@@ -1,39 +1,50 @@
 ﻿using EasySaveConsole.model;
+using EasySaveConsole.view;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using EasySaveConsole.logger;
 
 namespace EasySaveConsole.controller
 {
     class ControllerSave
     {
-        private ControllerLogger controllerLogger;
-        private ModelSave[] modelsSave;
+        private List<ModelSave> modelSaves;
         private ViewConsole view;
+        private LogLogger logLogger;
+        private StateLogger stateLogger;
 
-        public ControllerSave(ModelSave[] modelsSave, ViewConsole view)
+        public ControllerSave(List<ModelSave> modelSaves, ViewConsole view)
         {
-            throw new NotImplementedException();
+            this.modelSaves = modelSaves;
+            this.view = view;
+            logLogger = LogLogger.getInstance();
+            stateLogger = StateLogger.getInstance();
         }
 
         public void run()
         {
+            throw new NotImplementedException();
         }
 
         private void createNewSave()
         {
+            throw new NotImplementedException();
         }
 
         private void deleteSave(int saveNumber)
         {
+            throw new NotImplementedException();
         }
 
         private void runAllSaves()
         {
+            throw new NotImplementedException();
         }
 
         private void runSave(int saveNumber)
         {
+            throw new NotImplementedException();
         }
     }
 }
