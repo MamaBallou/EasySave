4b2752ea8368f693384ea5aaeb2c2e7bc4290aa7
Brenda <74040397+BrendaDJIGUEMDE@users.noreply.github.com>
2021-11-25 11:47:09 +0100

    Create New Save

--- EasySaveConsole/EasySaveConsole/controller/ControllerSave.cs
+++ EasySaveConsole/EasySaveConsole/controller/ControllerSave.cs
@@ -38,7 +38,68 @@ namespace EasySaveConsole.controller
 
         private void createNewSave()
         {
-            throw new NotImplementedException();
+                       //TODO: get input (view) in sourceFile, targetFile and save type
+            //get number of saves
+            int Nb = modelSaves.Count;
+            //get path access
+            bool sourceAccess = Tool.getInstance().checkExistance(sourceFile);
+            bool targetAccess = Tool.getInstance().checkExistance(targetFile);
+
+            ///////////case number of saves <5
+            if (Nb <5)
+            {
+                try
+                {
+                
+                            //if mode save is differential create differential save
+                            //TODO: enter differential condition
+                            if (true)
+                            {
+                                
+                                ModelSaveDifferential TypeDifferential = new ModelSaveDifferential("", @"", @"");
+                                ModelState modelState = new ModelState("name", "source", "target");
+                                TypeDifferential.save(ref modelState);
+                                modelSaves.Add(TypeDifferential);
+                                Console.WriteLine("the save has been created succefull");
+                            }
+
+                            //if mode save is total create total save
+                            //TODO: enter total condition
+                            if (true)
+                            {
+                                ///check the number of file in directory to save
+                                int NbFileToSave = Directory.GetFiles(sourceFile.ToString()).Length;
+                                //check if the Files number to save + the saves number which exist is not more than 5
+                                //if Files number to save + the saves number > 5
+                                if (NbFileToSave + Nb > 5)
+                                {
+                                    int NbFileCanSave = 5 - Nb;
+                                    Console.WriteLine("Error you can't save more than 5!!!! the number of save is so much, you can only add {0} ", NbFileCanSave);
+                                }
+                                //if Files number to save + the saves number <= 5
+                                else
+                                {
+                                    ModelSaveTotal TypeTotal = new ModelSaveTotal("", @"", @"");
+                                    ModelState modelState = new ModelState("name", "source", "target");
+                                    TypeTotal.save(ref modelState);
+                                    modelSaves.Add(TypeTotal);
+                                    Console.WriteLine("the save has been created succefull");
+
+                                }
+                            }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                //if number of save >= 5
+                Console.WriteLine("ERROR!!! you can't create a new save, the number of save is already 5 ");
+
+            }
         }
 
         private void deleteSave(int saveNumber)
