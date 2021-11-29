b148f0d3c9cbbe2e42e59c831ab7e729707ea5b3
Mathilde Ballouhey <mathildeballouhey.nausikaa44@gmail.com>
2021-11-29 20:30:51 +0100

    add : Test about futur new function in ModelSave

---
+++ EasySaveTest/model/ModelSaveTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using EasySaveConsole.model;
+using NUnit.Framework;
+
+namespace EasySaveTest.model
+{
+    internal class ModelSaveTest
+    {
+        const string SourcePath = "../../../test_files/MyDir/";
+        const string subdir = "subdir/";
+        const string TargetPath = "../../../test_files/archive/";
+        const string FileName = "testSave.txt";
+        const string SaveName = "save1";
+
+        [SetUp]
+        public void Setup()
+        {
+            // Create a source path
+            // Try to create the directory.
+            Directory.CreateDirectory(SourcePath);
+            File.WriteAllText(@String.Concat(SourcePath, "/", FileName), "Hello");
+            // Create SubDir
+            Directory.CreateDirectory(@String.Concat(SourcePath, subdir));
+            File.WriteAllText(String.Concat(@String.Concat(SourcePath, subdir, "/", FileName)), "World");
+            // Delete target path to be sure that save function create target path
+            try
+            {
+                Directory.Delete(TargetPath, true);
+            }
+            catch { }
+        }
+
+        [Test]
+        public void testSaveDirectory()
+        {
+            ModelState modelState = new ModelState(SaveName, SourcePath, TargetPath);
+            ModelSave modelSave = new ModelSaveDifferential(SaveName, SourcePath, TargetPath);
+            modelSave.saveFolder(SourcePath, TargetPath, ref modelState);
+            Assert.IsTrue(Directory.Exists(@String.Concat(TargetPath, SaveName, "/initial")));
+            Assert.IsTrue(File.Exists(@String.Concat(TargetPath, SaveName, "/initial/", FileName)));
+            Assert.IsTrue(Directory.Exists(@String.Concat(TargetPath, SaveName, "/initial/", subdir)));
+            Assert.IsTrue(File.Exists(@String.Concat(TargetPath, SaveName, "/initial/", subdir, FileName)));
+        }
+    }
+}
