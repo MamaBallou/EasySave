c2005f4726767718c765e0e39e3e3d10f69271d4
sbriffaud <sbriffaud@LAPTOP-K0VA6E62.localdomain>
2021-12-05 20:15:21 +0100

    [v2.0] Added a Prosoft logo, a menu icon, and some juicy animation

--- EasySaveGUI/EasySaveGUI/MainWindow.xaml.cs
+++ EasySaveGUI/EasySaveGUI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -42,7 +43,7 @@ namespace EasySaveGUI
         /// <param name="e"></param>
         private void close_MouseEnter(object sender, MouseEventArgs e)
         {
-            close.Foreground = (Brush)new BrushConverter().ConvertFrom("#aa4f0a");
+            close.Foreground = (Brush)new BrushConverter().ConvertFrom("#5c5e5b");
             close.Cursor = (Cursor)new CursorConverter().ConvertFromString(CursorType.Hand.ToString());
         }
 
@@ -54,6 +55,26 @@ namespace EasySaveGUI
         private void close_MouseLeave(object sender, MouseEventArgs e)
         {
             close.Foreground = Brushes.Black;
+        }
+
+        private void menu_logo_MouseEnter(object sender, MouseEventArgs e)
+        {
+            menu_logo.Source = (ImageSource) new ImageSourceConverter().ConvertFromString("../../../logo_small_icon_only_inverted.png");
+        }
+
+        private void menu_logo_MouseLeave(object sender, MouseEventArgs e)
+        {
+            menu_logo.Source = (ImageSource)new ImageSourceConverter().ConvertFromString("../../../logo_small_icon_only.png");
+        }
+
+        private void menu_logo_MouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            menu_logo.Source = (ImageSource)new ImageSourceConverter().ConvertFromString("../../../logo_small_icon_only.png");
+        }
+
+        private void menu_logo_MouseLeftButtonUp(object sender, MouseEventArgs e)
+        {
+            menu_logo.Source = (ImageSource)new ImageSourceConverter().ConvertFromString("../../../logo_small_icon_only_inverted.png");
         }
     }
 }
