adfa9c60a0d780d029303e43c827b7cb5a890125
Mathilde Ballouhey <mathildeballouhey.nausikaa44@gmail.com>
2021-12-14 00:45:30 +0100

    Setting encrypted extension

--- EasySaveGUI/Views/SettingsView.xaml.cs
+++ EasySaveGUI/Views/SettingsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+using EasySaveGUI.viewmodel;
 
 namespace EasySaveGUI.Views
 {
@@ -7,9 +9,26 @@ namespace EasySaveGUI.Views
     /// </summary>
     public partial class SettingsView : UserControl
     {
+        private ViewModelSettingsView viewModel;
         public SettingsView()
         {
+            viewModel = ViewModelSettingsView.GetInstance();
+            DataContext = viewModel;
             InitializeComponent();
+        }
+
+        private void SaveClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            List<CheckBox> checkBoxs = new List<CheckBox> { cbx_pdf, cbx_jpg, cbx_docx, cbx_txt, cbx_exe };
+            List<string> toEncrypt = new List<string>();
+            checkBoxs.ForEach(checkBox =>
+            {
+                if(checkBox.IsChecked == true)
+                {
+                    toEncrypt.Add(checkBox.Content.ToString());
+                }
+            });
+            viewModel.SetToEncryptExtension(toEncrypt);
         }
     }
 }
